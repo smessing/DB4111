2e23e07fd33c666e0776681a5ced97432ae6f37c
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-02-29 17:13:21 -0500

    adding users insert statements

---
+++ data/sql/users_insert_statements.sql
@@ -0,0 +1,49 @@
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('ben@fun.com', 'Ben', 'abc123', '1hgf');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('dan@fun.com', 'Dan', 'a123', '2werw');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('carl@fun.com', 'Carl', 'b123', '3ytr');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('clarence@fun.com', 'Clarence', 'c123', 'hgfh4');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('abby@fun.com', 'Abby', 'd123', '5tretre');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('Hannah@fun.com', 'Hannah', 'e123', '6hgfhfg');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('Sam@fun.com', 'Sam', 'f123', 'hgfhgf7');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('David@fun.com', 'David', '1g23', '8tr');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('debbie@fun.com', 'Debbie', '1fdsf23', 'fds9');
+
+INSERT INTO Users
+(email, displayName, password, passwordSalt)
+VALUES
+('Martin@fun.com', 'Martin', '123fdsfsd', '1sf0');
