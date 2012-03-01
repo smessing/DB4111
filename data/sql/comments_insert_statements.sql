fc46a84bdc6e8e35c325f7f474010d34da33f126
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-02-29 19:10:25 -0500

    fixing comments emails and adding donation statements

--- data/sql/comments_insert_statements.sql
+++ data/sql/comments_insert_statements.sql
@@ -1,59 +1,59 @@
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('1','83e5cc1b9c24f749b19e923cfd082faa','I hate this project!','24-apr-11','someGuy@aol.com' );
+('1','83e5cc1b9c24f749b19e923cfd082faa','I hate this project!','24-apr-11','clarence@fun.com' );
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('2','6980e0f25fdb0801c1881270afbc1b4c','I love this project!','02-apr-11','al@aol.com' );
-
-
-INSERT INTO Comments_ABOUT
-(tid, pid, comments, cDate, email)
-VALUES
-('3','d6ea9d010ce505c40fb0dbebef83a850','fun fun fun...','24-mar-10','bill@aol.com');
+('2','6980e0f25fdb0801c1881270afbc1b4c','I love this project!','02-apr-11','abby@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('4','3607c8cfe6b6902a4e1790cb3699a56e','top notch work, Glenn!','04-may-11','someGuy@aol.com' );
-
-
--- here
-INSERT INTO Comments_ABOUT
-(tid, pid, comments, cDate, email)
-VALUES
-('5','cd4ad25dd96fe0c3c6d5d007ea185b8a','These kids are great!','24-jan-11','phile@yahoo.com' );
+('3','d6ea9d010ce505c40fb0dbebef83a850','fun fun fun...','24-mar-10','Hannah@fun.com');
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('6','51a492ce24475c39dda69f623346ceb3','The good guys win again!','11-dec-11','charles@aol.com' );
+('4','3607c8cfe6b6902a4e1790cb3699a56e','top notch work, Glenn!','04-may-11','Sam@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('7','874cd2d6ec1efea4896ed5a400b3eb64','don\'t donate. its a scam!','24-apr-11','someGuy@aol.com' );
+('5','cd4ad25dd96fe0c3c6d5d007ea185b8a','These kids are great!','24-jan-11','David@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('8','98e6156cf61ded411bc49b44b81917af','crazy stuff','08-apr-11','someGuy@aol.com' );
+('6','51a492ce24475c39dda69f623346ceb3','The good guys win again!','11-dec-11','debbie@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('9','fe5e4d23c8f6c5fe33a68b6515b1cc03','cannot wait to see the end results!','03-jun-11','tim@aol.com' );
+('7','874cd2d6ec1efea4896ed5a400b3eb64','don\'t donate. its a scam!','24-apr-11','Martin@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('10','6a199fe68aced29ee610f811f6f663dd','stop this at once','03-apr-11','someGuy@aol.com' );
+('8','98e6156cf61ded411bc49b44b81917af','crazy stuff','08-apr-11','ben@fun.com' );
+
+
+INSERT INTO Comments_ABOUT
+(tid, pid, comments, cDate, email)
+VALUES
+('9','fe5e4d23c8f6c5fe33a68b6515b1cc03','cannot wait to see the end results!','03-jun-11','dan@fun.com' );
+
+
+INSERT INTO Comments_ABOUT
+(tid, pid, comments, cDate, email)
+VALUES
+('10','6a199fe68aced29ee610f811f6f663dd','stop this at once','03-apr-11','carl@fun.com' );
+
 
---
+++ data/sql/donations_insert_statements.sql
@@ -0,0 +1,58 @@
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('1','83e5cc1b9c24f749b19e923cfd082faa',100,'24-apr-11','clarence@fun.com', 1);
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('2','6980e0f25fdb0801c1881270afbc1b4c',3000,'02-apr-11','abby@fun.com', 2);
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('3','d6ea9d010ce505c40fb0dbebef83a850',50,'24-mar-10','Sam@fun.com',3);
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('4','3607c8cfe6b6902a4e1790cb3699a56e',20,'04-may-11','David@fun.com',4 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('5','cd4ad25dd96fe0c3c6d5d007ea185b8a',25,'24-jan-11','debbie@fun.com',5 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('6','51a492ce24475c39dda69f623346ceb3',45,'11-dec-11','ben@fun.com',6 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('7','874cd2d6ec1efea4896ed5a400b3eb64',55,'24-apr-11','dan@fun.com', 7 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('8','98e6156cf61ded411bc49b44b81917af',90,'08-apr-11',140, 'ben@fun.com', 8 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('9','fe5e4d23c8f6c5fe33a68b6515b1cc03',120,'03-jun-11','carl@fun.com',9 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('10','6a199fe68aced29ee610f811f6f663dd',250,'03-apr-11','Hannah@fun.com', 10 );
+
