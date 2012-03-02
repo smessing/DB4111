a44d1e6cafa4af0cedb95826b4aef62651408799
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-03-02 12:52:34 -0500

    fixed comments sql statements

--- data/sql/comments_insert_statements.sql
+++ data/sql/comments_insert_statements.sql
@@ -1,59 +1,56 @@
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('1','83e5cc1b9c24f749b19e923cfd082faa','I hate this project!','24-apr-11','clarence@fun.com' );
+('a89af066d9553a4aff50b0dc5c3650ea','83e5cc1b9c24f749b19e923cfd082faa','I hate this project!','24-apr-11','clarence@fun.com' );
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('2','6980e0f25fdb0801c1881270afbc1b4c','I love this project!','02-apr-11','abby@fun.com' );
-
-
-INSERT INTO Comments_ABOUT
-(tid, pid, comments, cDate, email)
-VALUES
-('3','d6ea9d010ce505c40fb0dbebef83a850','fun fun fun...','24-mar-10','Hannah@fun.com');
+('b69c5a29518b74cd8d762edc5f9d1f42','6980e0f25fdb0801c1881270afbc1b4c','I love this project!','02-apr-11','abby@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('4','3607c8cfe6b6902a4e1790cb3699a56e','top notch work, Glenn!','04-may-11','Sam@fun.com' );
+('1f73d21635cf052f73bd1324cb841b77','d6ea9d010ce505c40fb0dbebef83a850','fun fun fun...','24-mar-10','Hannah@fun.com');
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('5','cd4ad25dd96fe0c3c6d5d007ea185b8a','These kids are great!','24-jan-11','David@fun.com' );
+('300e15987fbc2ef77117f3625cb58ead','f27e2b95d333b391e8bae21d52223ef3','top notch work, Glenn!','04-may-11','Sam@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('6','51a492ce24475c39dda69f623346ceb3','The good guys win again!','11-dec-11','debbie@fun.com' );
+('d2880ab6b34da8740fb48a5547a26510','cd4ad25dd96fe0c3c6d5d007ea185b8a','These kids are great!','24-jan-11','David@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('7','874cd2d6ec1efea4896ed5a400b3eb64','don\'t donate. its a scam!','24-apr-11','Martin@fun.com' );
+('79c1dc3ed623e5b22d649a9b4db77f6d','526f5d0794229b2bff326ced6cf10099','The good guys win again!','11-dec-11','debbie@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('d75e2122dcdf69bec7059840bb8e81ee','98e6156cf61ded411bc49b44b81917af','crazy stuff','08-apr-11','ben@fun.com' );
+('d2880ab6b34da8740fb48a5547a26510','51a492ce24475c39dda69f623346ceb3','dont donate. its a scam!','24-apr-11','Martin@fun.com' );
+
+INSERT INTO Comments_ABOUT
+(tid, pid, comments, cDate, email)
+VALUES
+('1f73d21635cf052f73bd1324cb841b77','0ded9463538dc1a3a763f70d4b7ad046','good stuff','24-apr-11','ben@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('9','fe5e4d23c8f6c5fe33a68b6515b1cc03','cannot wait to see the end results!','03-jun-11','dan@fun.com' );
+('b69c5a29518b74cd8d762edc5f9d1f42','874cd2d6ec1efea4896ed5a400b3eb64','cannot wait to see the end results!','03-jun-11','dan@fun.com' );
 
 
 INSERT INTO Comments_ABOUT
 (tid, pid, comments, cDate, email)
 VALUES
-('10','6a199fe68aced29ee610f811f6f663dd','stop this at once','03-apr-11','carl@fun.com' );
-
-
+('8a9f9b4ba35841309b7e68afe4e324a1','fe5e4d23c8f6c5fe33a68b6515b1cc03','stop this at once','03-apr-11','carl@fun.com' );
