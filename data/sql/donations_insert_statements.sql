8eaf7d226080beed0006624f448cc33fe398a508
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-03-02 13:00:23 -0500

    updated donations insert statements

--- data/sql/donations_insert_statements.sql
+++ data/sql/donations_insert_statements.sql
@@ -1,58 +1,58 @@
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('1','83e5cc1b9c24f749b19e923cfd082faa',100,'24-apr-11','clarence@fun.com', 1);
+('a89af066d9553a4aff50b0dc5c3650ea','83e5cc1b9c24f749b19e923cfd082faa',100,'24-apr-11','clarence@fun.com', 1);
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('2','6980e0f25fdb0801c1881270afbc1b4c',3000,'02-apr-11','abby@fun.com', 2);
-
-
-INSERT INTO Donations_FUND
-(tid, pid, amount, donationDate, email, did)
-VALUES
-('3','d6ea9d010ce505c40fb0dbebef83a850',50,'24-mar-10','Sam@fun.com',3);
+('b69c5a29518b74cd8d762edc5f9d1f42','6980e0f25fdb0801c1881270afbc1b4c',3000,'02-apr-11','abby@fun.com', 2);
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('4','3607c8cfe6b6902a4e1790cb3699a56e',20,'04-may-11','David@fun.com',4 );
+('1f73d21635cf052f73bd1324cb841b77','d6ea9d010ce505c40fb0dbebef83a850',50,'24-mar-10','Sam@fun.com',3);
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('5','cd4ad25dd96fe0c3c6d5d007ea185b8a',25,'24-jan-11','debbie@fun.com',5 );
+('300e15987fbc2ef77117f3625cb58ead','f27e2b95d333b391e8bae21d52223ef3',20,'04-may-11','David@fun.com',4 );
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('6','51a492ce24475c39dda69f623346ceb3',45,'11-dec-11','ben@fun.com',6 );
+('d2880ab6b34da8740fb48a5547a26510','cd4ad25dd96fe0c3c6d5d007ea185b8a',25,'24-jan-11','debbie@fun.com',5 );
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('7','874cd2d6ec1efea4896ed5a400b3eb64',55,'24-apr-11','dan@fun.com', 7 );
+('79c1dc3ed623e5b22d649a9b4db77f6d','526f5d0794229b2bff326ced6cf10099',45,'11-dec-11','ben@fun.com',6 );
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('8','98e6156cf61ded411bc49b44b81917af',90,'08-apr-11',140, 'ben@fun.com', 8 );
+('d2880ab6b34da8740fb48a5547a26510','51a492ce24475c39dda69f623346ceb3',55,'24-apr-11','dan@fun.com', 7 );
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('9','fe5e4d23c8f6c5fe33a68b6515b1cc03',120,'03-jun-11','carl@fun.com',9 );
+('1f73d21635cf052f73bd1324cb841b77','0ded9463538dc1a3a763f70d4b7ad046',90,'08-apr-11', 'ben@fun.com', 8 );
 
 
 INSERT INTO Donations_FUND
 (tid, pid, amount, donationDate, email, did)
 VALUES
-('10','6a199fe68aced29ee610f811f6f663dd',250,'03-apr-11','Hannah@fun.com', 10 );
+('b69c5a29518b74cd8d762edc5f9d1f42','874cd2d6ec1efea4896ed5a400b3eb64',120,'03-jun-11','carl@fun.com',9 );
+
+
+INSERT INTO Donations_FUND
+(tid, pid, amount, donationDate, email, did)
+VALUES
+('b69c5a29518b74cd8d762edc5f9d1f42','874cd2d6ec1efea4896ed5a400b3eb64',250,'03-apr-11','Hannah@fun.com', 10 );
 
