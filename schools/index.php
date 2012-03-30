e027291e81add6ea3517cdf4e174c723ab43a34e
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 18:10:57 -0400

    starting users page.

--- schools/index.php
+++ schools/index.php
@@ -35,13 +35,13 @@
 	echo '<b>Address</b>: ' . $res[3] . ' ' . $res[4] . ', ' . $res[5] . ', ' . $res[6] . "\n";
 	echo "<br/>\n";
 	echo "<ul class='toc'>\n";
-        echo '<li><span><b>Average Class Size</b></span><span>' . $res[7] . "</span></li>\n";
+        echo '<li><span><b>Average Class Size</b></span><span>' . number_format($res[7] * 100, 0, ".", "") . " Students</span></li>\n";
 	echo '<li><span><b>Poverty Level</b></span><span>' . $res[8] . "</span></li>\n";
 	echo '<li><span><b>Average Math SAT Score</b></span><span>' . $res[9] . "/800</span></li>\n";
 	echo '<li><span><b>Average Reading SAT Score</b></span><span>' . $res[10] . "/800</span></li>\n";
 	echo '<li><span><b>Average Writing SAT Score</b></span><span>' . $res[11] . "/800</span></li>\n";
-	echo '<li><span><b>Graduation Rate</b></span><span>' . $res[12] . "</span></li>\n";
-	echo '<li><span><b>Percent of AP Scores Above 2</b></span><span>' . $res[13] . "</span></li>\n";
+	echo '<li><span><b>Graduation Rate</b></span><span>' . number_format($res[12] * 100, 0, ".", "") . "%</span></li>\n";
+	echo '<li><span><b>Percent of AP Scores Above 2</b></span><span>' . number_format($res[13] * 100, 0, ".", "") . "%</span></li>\n";
         echo "</ul>\n";	
       }
 
@@ -66,7 +66,9 @@
          echo "<ul class='toc'>\n";
            echo "<li><span><b>Expiration Date</b></span><span>" . $res[4] . "</span></li>\n";          
            echo "<li><span><b>Number of Students Involved</b></span><span>" . $res[12] . "</span></li>\n";
-           echo "<li><span><b>Subject</b></span><span>" . $res[7] . "</span></li>\n";
+           if (!empty($res[7])) {
+             echo "<li><span><b>Subject</b></span><span>" . str_replace("  ", " & ", $res[7]) . "</span></li>\n";
+           }
          echo "</ul>\n";
        echo "</li>\n";
       }
