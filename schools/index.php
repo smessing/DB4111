ae5fe01f983fc4974daa1f08e2750b6c55d88188
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 15:27:47 -0400

    working on school profile page.

--- schools/index.php
+++ schools/index.php
@@ -6,21 +6,36 @@
   <?php
     $id=$_REQUEST['id'];
 
-    if(empty($id)) {
-
+    if (empty($id)) {
+	echo 'ERROR: Must specify a school id!';
     }
 
-    $requestStr = "select * " .
-		  "from schools_s_in_s_have s " .
-                  "where s.ncesid=" . $id;
+    if (!empty($id)) {
 
-    // Connect to DB
+      $requestStr= "select s.name, s.latitude, s.longitude, a.streetNumber, " .
+                           "a.streetname, a.zipcode, a.bname, s.avgclasssize, " .
+                           "s.povertylevel, s.avgmathsatscore, s.avgreadingsatscore, " .
+                           "s.avgwritingsatscore, s.graduationrate, s.percentapabove2, s.ncesid " .
+                   "from schools_s_in_s_have s, addresses a " .
+                   "where s.latitude=a.latitude and s.longitude=a.longitude " .
+                           "and s.ncesid=" . $id;
 
-    ini_set('display_errors', 'On');
-    $db = 
+      // Connect to DB
 
-    echo $id;
+      ini_set('display_errors', 'On');
+      $db = 'w4111f.cs.columbia.edu:1521/adb'; 
+      $conn = oci_connect("sbm2158", "donorschoose", $db);
+
+      header("Content-type: text/html");
+      $stmt = oci_parse($conn, $requestStr);
+      oci_execute($stmt, OCI_DEFAULT);
+      while($res = oci_fetch_row($stmt)) {
+        echo '<h1>' . $res[0] . '</h1>';   
+	echo '<h2>Profile</h2>';
+	echo '<h3>' . $res[3] . ' ' . $res[4] . ', ' . $res[5] . ', ' . $res[6] . '</h3>';
+      }
+
+    }
   ?>
-<h2>Test</h2>
 </body>
 </html>
