11c74f82a902815e103d5e46a3cb429fccc5edc0
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-03-30 16:57:45 -0400

    sql statements in projects

--- projects/index.php
+++ projects/index.php
@@ -14,10 +14,10 @@
 
       $requestStr= "select p.title, p.subject, p.shortDescription " . 
                    "from projects_propose_at p, schools_s_in_s_have s, " .
-                   "addresses a " .
-                   "where p.pid=" . $id . " and " .
-                   "p.ncesid=s.ncesid and s.latitude=a.latitude and " .
-                   "s.longitude=a.longitude;";
+                         "addresses a " .
+                   "where p.pid=" . $id . " and p.ncesid=s.ncesid " .
+                          "and s.latitude=a.latitude and " .
+                          "s.longitude=a.longitude;";
 
       // Connect to DB
 
