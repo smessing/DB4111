873733289575d5809e5133cb211e7f050974738f
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 18:32:36 -0400

    working on signup.

--- static/php/footer.php
+++ static/php/footer.php
@@ -1,4 +1,4 @@
 <footer>
   <hr noshade/>
-  <a href="../index.html">Main Page</a> | <a href="../users/sign_up.php">Sign Up</a>
+  <a href="../index.html">Main Page</a> | <a href="../users/sign_up.php">Sign Up</a> | <a href="http://www.donorschoose.org/">Donors Choose</a>
 </footer>
--- users/sign_up.php
+++ users/sign_up.php
@@ -5,6 +5,14 @@
 <body>
 <h2>Create an Account</h2>
 
+<form action="create_user.php" method="post" align="center">
+E-mail: <input type="text" name="email" /> <br/>
+Display Name: <input type="text" name="name"/> <br/>
+Password: <input type="password" name="pass" /> <br/>
+Password (confirmation): <input type="password" name="pass" /> <br/>
+<input type="submit"/>
+</form>
+
 <?php include("../static/php/footer.php"); ?>
 </body>
 </html>
