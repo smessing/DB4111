f5f967532ba8a39d6fb92c2f5346e48923c1d3be
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 14:51:38 -0400

    refactoring, starting schools index.

---
+++ schools/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+  <link href="../code/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <?php
+    $id=$_REQUEST['id'];
+    echo $id;
+  ?>
+<h2>Test</h2>
+</body>
+</html>
