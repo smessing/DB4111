cd20b8e5ccefb23b807d474662812eeac66513c0
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 16:31:09 -0400

    adding project display on school page.

--- schools/index.php
+++ schools/index.php
@@ -30,18 +30,39 @@
       $stmt = oci_parse($conn, $requestStr);
       oci_execute($stmt, OCI_DEFAULT);
       while($res = oci_fetch_row($stmt)) {
-        echo '<h1>' . $res[0] . '</h1>';   
-	echo '<h2>Profile</h2>';
-	echo '<b>Address</b>: ' . $res[3] . ' ' . $res[4] . ', ' . $res[5] . ', ' . $res[6];
-	echo '<br/>';
-	echo '<ul class=\'toc\'>';
-        echo '<li><span><b>Average Class Size</b></span><span>' . $res[7] . '</span></li>';
-	echo '<li><span><b>Poverty Level</b></span><span>' . $res[8] . '</span></li>';
-	echo '<li><span><b>Average Math SAT Score</b></span><span>' . $res[9] . '/800';
-	echo '<li><span><b>Average Reading SAT Score</b></span><span>' . $res[10] . '/800';
-	echo '<li><span><b>Average Writing SAT Score</b></span><span>' . $res[11] . '/800';
-        echo '</ul>';	
+        echo '<h1>' . $res[0] . "</h1>\n";   
+	echo "<h2>Profile</h2>\n";
+	echo '<b>Address</b>: ' . $res[3] . ' ' . $res[4] . ', ' . $res[5] . ', ' . $res[6] . "\n";
+	echo "<br/>\n";
+	echo "<ul class='toc'>\n";
+        echo '<li><span><b>Average Class Size</b></span><span>' . $res[7] . "</span></li>\n";
+	echo '<li><span><b>Poverty Level</b></span><span>' . $res[8] . "</span></li>\n";
+	echo '<li><span><b>Average Math SAT Score</b></span><span>' . $res[9] . "/800</span></li>\n";
+	echo '<li><span><b>Average Reading SAT Score</b></span><span>' . $res[10] . "/800</span></li>\n";
+	echo '<li><span><b>Average Writing SAT Score</b></span><span>' . $res[11] . "/800</span></li>\n";
+	echo '<li><span><b>Graduation Rate</b></span><span>' . $res[12] . "</span></li>\n";
+	echo '<li><span><b>Percent of AP Scores Above 2</b></span><span>' . $res[13] . "</span></li>\n";
+        echo "</ul>\n";	
       }
+
+      echo "<h2>Projects</h2>\n";
+
+      $requestStr = "select p.pid, p.fundURL, p.fundingStatus, p.fulfillmentTrailer," .
+                           "p.expirationDate, p.totalPrice, p.title, p.subject, p.shortDescription," .
+                           "p.proposalURL, p.percentFunded, p.imageURL, p.numStudents, p.tid, p.ncesID " .
+                    "from projects_propose_at p " .
+                    "where p.ncesID=" . $id;
+      $stmt = oci_parse($conn, $requestStr);
+      oci_execute($stmt, OCI_DEFAULT);
+      echo "<ul>\n";
+      while($res = oci_fetch_row($stmt)) {
+         echo "<li><b>" . $res[6] . "</b>";
+             echo "<ul class='toc'>\n";
+    
+             echo "</ul>\n";
+	 echo "</li>\n";
+      }
+      echo "</ul>\n";
 
     }
   ?>
