5e7687d3cc8e7d8fbb051d39760aed8414f79e00
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 19:06:24 -0400

    working on sign_up

--- users/sign_up.php
+++ users/sign_up.php
@@ -8,8 +8,13 @@
 <form action="create_user.php" method="post" align="center">
 E-mail: <input type="text" name="email" /> <br/>
 Display Name: <input type="text" name="name"/> <br/>
+<?php
+  if ($_REQUEST['error'] == 'pass') {
+     echo "<font color='red'>Password fields must match.</font><br/>";
+  }
+?>
 Password: <input type="password" name="pass" /> <br/>
-Password (confirmation): <input type="password" name="pass" /> <br/>
+Password (confirmation): <input type="password" name="cpass" /> <br/>
 <input type="submit"/>
 </form>
 
