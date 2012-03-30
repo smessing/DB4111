fcceea58dd888bbb380db86a7f9a2cccfb193892
Samuel Messing <samuel.messing@gmail.com>
2012-03-30 18:14:34 -0400

    going to get footer.php working.

---
+++ users/sign_up.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <link href="../code/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<h2>Create an Account</h2>
+
+<?php include("../static/php/footer.php"); ?>
+</body>
+</html>
