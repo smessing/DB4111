675f95007cda8dbe895307654710a1a977dda790
Benjamin Rapaport <benjamin.rapaport@gmail.com>
2012-04-04 10:20:34 -0400

    fixing projects

--- projects/index.php
+++ projects/index.php
@@ -31,43 +31,45 @@
       $stmt = oci_parse($conn, $requestStr);
       oci_execute($stmt, OCI_DEFAULT);
       while($res = oci_fetch_row($stmt)) {
-        
+
         // HEADER SECTION
         // p.title
         echo "<h1>" . $res[0] . "</h1>\n";
         // p.shortDescription
-        echo "<p>" . $res[3] . "</p>\n";
+        echo "<p>" . $res[3] . "</p>\n"; 
 
         // PROJECT OVERVIEW SECTION
         echo "<h2>Project Overview</h2>\n"; 
-        echo "<ul>\n";
+        echo "<ul class ='toc'>\n";
         // t.name
-        echo "<li><b>Teacher: </b>" . $res[2] . "</li>\n";
+        echo "<li><span><b>Teacher: </b></span><span>" . $res[2] . "</span></li>\n";
         // s.name
-        echo "<li><b>School: </b><a href=\"../schools/index.php?id=" . $res[8] . "\">" . $res[9] . "</a></li>\n";
-        // p.subject, if it has one
+        echo "<li><span><b>School: </b></span><span><a href=\"../schools/index.php?id=" . $res[8] . "\">" . $res[9] . "</a></span></li>\n";
+
+        // p.subject, if there is one
         if(!empty($res[1])) {
-          echo "<li><b>Subject: </b>" . $res[1] . "</li>\n";
+           echo "<li><span><b>Subject: </b></span><span>" . $res[1] . "</span></li>\n";
         }
-        // p.numStudents 
-        echo "<li><b>Number of Students: </b>" . $res[7] . "</li>\n"; 
+        
+        // p.numStudents
+        echo "<li><span><b>Number of Students: </b></span><span>" . $res[7] . "</span></li>\n"; 
         echo "</ul>\n";
 
-        // FUNDING SECTION
+        // FUNDING SECTION 
         echo "<h2>Funding</h2>\n"; 
-        echo "<ul>\n";
-
+        echo "<ul class ='toc'>\n";
+    
         // p.percentFunded
         // red font if funding below 15%
         if($res[6] < 0.15) {
-          echo "<li><b>Percent Funded: </b><font color=\"red\">" .  number_format($res[6]*100,0,".","") . "%</font></li>\n"; }
+          echo "<li><span><b>Percent Funded: </b></span><span><font color=\"red\">" .  number_format($res[6]*100,0,".","") . "%</font></span></li>\n"; }
         else {
-          echo "<li><b>Percent Funded: </b>" .  number_format($res[6]*100,0,".","") . "%</li>\n"; }
+          echo "<li><span><b>Percent Funded: </b></span><span>" .  number_format($res[6]*100,0,".","") . "%</span></li>\n"; }
 
         // p.totalPrice
-        echo "<li><b>Total Funding Requested: </b>$" . $res[5] . "</li>\n";
+        echo "<li><span><b>Total Funding Requested: </b></span><span>$" . $res[5] . "</span></li>\n"; 
         // p.expirationDate
-        echo "<li><b>Last Day to Donate: </b>" . $res[4] . "</li>\n"; 
+        echo "<li><span><b>Last Day to Donate: </b></span><span>" . $res[4] . "</span></li>\n"; 
         echo "</ul>\n";
       }
 
@@ -80,4 +82,4 @@
   <a href="../index.html">Main Page</a>
 </footer>
 </body>
-</html>gra
+</html>
