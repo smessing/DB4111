b1f563ed6e2c39e639ed2aa556bac89ec0cc97b1
Samuel Messing <samuel.messing@gmail.com>
2012-04-04 15:13:12 -0400

    removing junk.

--- static/php/message.php
+++ static/php/message.php
@@ -2,7 +2,7 @@
 // check for messages on this page, print those message:
 if (!empty($_REQUEST['msg'])) {
   $msg = $_REQUEST['msg'];
-  echo "<div class=message><p><font color='green'>";
+  echo "<div class=message><p>";
   if ($msg == 'welcome') {
     echo "Welcome to the site!";
   }
--- users/log_in_user.php
+++ users/log_in_user.php
@@ -15,7 +15,7 @@
   echo "<br/>";
   echo "---submitted_pass" . var_dump($submitted_pass);
   
-  $regex = "^[a-zA-Z0-9\._\-\+]+@[a-zA-Z0-9\._\-]+\.[a-zA-Z]{2,4}$/";
+  $regex = "/^[a-zA-Z0-9\._\-\+]+@[a-zA-Z0-9\._\-]+\.[a-zA-Z]{2,4}$/";
 
   // check that e-mail is valid e-mail:
   if (0 == preg_match($regex, $submitted_email)) {
