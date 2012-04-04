1bb490ac8e798952bd807c58e8c55af8324c61e6
Samuel Messing <samuel.messing@gmail.com>
2012-04-04 19:31:12 -0400

    working on getting logging out to work.

--- static/php/footer.php
+++ static/php/footer.php
@@ -1,7 +1,14 @@
 <footer>
   <hr noshade/>
-  <a href="../index.php">Main Page</a> | <a href="../users/sign_up.php">Sign Up</a> | <a href="../users/log_in.php">Log In</a> | <a href="http://www.donorschoose.org/">Donors Choose</a><br/>
   <?php
+    echo "<a href='../index.php'>Main Page</a> | ";
+    if (isset($_SESSION['email'])) {
+      echo "<a href='log_out.php'>Log Out</a> | ";
+    } else {
+      echo "<a href='log_in.php'>Log in</a> | ";
+      echo "<a href='sign_up.php'>Sign Up</a> | ";
+    }
+    echo "<a href='http://www.donorschoose.org/'>Donors Choose</a><br/>";
     if (isset($_SESSION['email'])) {
       echo "Logged in as: <a href='profile.php?email=" . $_SESSION['email'] .
            "'>" . $_SESSION['email'] . "</a>";
