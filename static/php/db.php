592ee6d14fa42c32369d8ffe630a6d1f2345a2ce
Samuel Messing <samuel.messing@gmail.com>
2012-04-05 11:38:05 -0400

    adding sanitization to getMultipleRows.

--- static/php/db.php
+++ static/php/db.php
@@ -12,16 +12,21 @@
     $resp = oci_fetch_row($stmt);
     // sanitize the resp of any HTML chars:
     for ($i = 0; $i < sizeof($resp); $i++) {
-      $resp[$i] = sanitizeHTML($resp[$i]);
+      $resp[$i] = sanitizeHTML(unsanitize($resp[$i]));
     }
     return $resp;
   }
 
   function getMultipleRows($requestStr, $conn) {
-    // !TODO! handle sanitation
     $stmt = oci_parse($conn, $requestStr);
     oci_execute($stmt);
     oci_fetch_all($stmt, $resp);
+    foreach($resp as $key => $value) {
+      for ($i = 0; $i < sizeof($value); $i++) {
+        $value[$i] = sanitizeHTML(unsanitize($value[$i]));
+      }
+      $resp[$key] = $value;
+    }
     return $resp;
   }
 
