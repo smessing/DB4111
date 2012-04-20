fbbc5d241bf8332a1986228ca913609f7466752c
Samuel Messing <samuel.messing@gmail.com>
2012-04-20 16:13:50 -0400

    final stuff.

--- data/sql/request_inserts.sql
+++ data/sql/request_inserts.sql
@@ -1,15 +1,40 @@
 insert into projects_request 
-(pid, tid, book, requestDate, requestQuant)
-values (
-'752asdfasdfe934', '428954', select book_typ (REF(b)) from book_typ_table b where b.isbn='0439708184', 1334950342, 50
-);
+select '752asdfasdfe934', '428954', ref(b), 1334950342, 50
+from book_typ_table b where b.isbn='0439708184';
 
+insert into projects_request 
+select '752asdfasdfe934', '428954', ref(b), 1334950347, 200
+from book_typ_table b where b.isbn='0439064872';
 
-INSERT INTO After_School_Programs_A_HAVE
-(aid, name, programType, agencyName, organizationName, elementaryLevel, 
-  middleSchoolLevel, highSchoolLevel, organizationPhoneNumber, 
-  latitude, longitude)
-VALUES
-('13', 'Community Association Progressive Dominicans', 'After-School Programs', 
-  'Beacon', 'MS 117', 'T', 'T', 'T', '718-466-1806', 
-  40.84854841840716, -73.90843506819625);
+insert into projects_request 
+select '752asdfasdfe934', '428954', ref(b), 1334000000, 100
+from book_typ_table b where b.isbn='0439064872';
+
+insert into projects_request 
+select '752asdfasdfe934', '428954', ref(b), 1334950881, 75
+from book_typ_table b where b.isbn='0262033844';
+
+insert into projects_request 
+select '752ae34', '428954', ref(b), 1334950123, 50
+from book_typ_table b where b.isbn='0439064872';
+
+insert into projects_request 
+select '752ae34', '428954', ref(b), 1334951234, 200
+from book_typ_table b where b.isbn='0439064872';
+
+insert into projects_request 
+select '752ae34', '428954', ref(b), 1334001111, 25
+from book_typ_table b where b.isbn='0439708184';
+
+insert into projects_request 
+select '752ae34', '428954', ref(b), 1334950111, 15
+from book_typ_table b where b.isbn='0439064872';
+
+insert into projects_request 
+select '752asdfasdfe93fdsdfs', '1111111', ref(b), 1334001113, 22
+from book_typ_table b where b.isbn='0131103628';
+
+insert into projects_request 
+select '752asdfasdfe93fdsdfs', '1111111', ref(b), 1334950114, 6
+from book_typ_table b where b.isbn='0262033844';
+
